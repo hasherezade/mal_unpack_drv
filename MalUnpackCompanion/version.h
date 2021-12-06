8889df7df85392be74551e4af27f165519ad40ec
hasherezade <hasherezade@gmail.com>
2021-12-06 09:28:46 -0800

    [VERSION] Incremented subversion

--- MalUnpackCompanion/version.h
+++ MalUnpackCompanion/version.h
@@ -1,4 +1,4 @@
 #pragma once
 
-#define VER_FILEVERSION             0,1,1,1
-#define VER_FILEVERSION_STR         "0.1.1.1\0"
+#define VER_FILEVERSION             0,1,1,2
+#define VER_FILEVERSION_STR         "0.1.1.2\0"
