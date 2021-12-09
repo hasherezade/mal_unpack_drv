b7dcdd75aa78b08583abae5fb998f80d4b73e611
hasherezade <hasherezade@gmail.com>
2021-12-08 17:21:31 -0800

    [VERSION] Updated subversion

--- MalUnpackCompanion/version.h
+++ MalUnpackCompanion/version.h
@@ -1,4 +1,4 @@
 #pragma once
 
-#define VER_FILEVERSION             0,1,1,3
-#define VER_FILEVERSION_STR         "0.1.1.3\0"
+#define VER_FILEVERSION             0,1,1,4
+#define VER_FILEVERSION_STR         "0.1.1.4\0"
