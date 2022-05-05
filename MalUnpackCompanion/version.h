b6c3c4248becbc8939743706442638f15098c8c6
hasherezade <hasherezade@gmail.com>
2022-05-05 12:39:08 -0700

    [VERSION] 0.1.1.15

--- MalUnpackCompanion/version.h
+++ MalUnpackCompanion/version.h
@@ -1,4 +1,4 @@
 #pragma once
 
-#define VER_FILEVERSION             0,1,1,13
-#define VER_FILEVERSION_STR         "0.1.1.13-h\0"
+#define VER_FILEVERSION             0,1,1,15
+#define VER_FILEVERSION_STR         "0.1.1.15\0"
